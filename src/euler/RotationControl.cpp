a340dd7cdadb9fc28205a419335654ca10b1e6f2
Robert Haschke <rhaschke@techfak.uni-bielefeld.de>
2015-10-25 10:53:58 +0100

    add frame to markers

--- src/euler/RotationControl.cpp
+++ src/euler/RotationControl.cpp
@@ -128,6 +128,30 @@ static visualization_msgs::msg::Marker createBoxMarker(double x, double y, doubl
 	return marker;
 }
 
+/// create a visual marker, here an arrow of given size (scale), direction, and color
+static visualization_msgs::msg::Marker createArrowMarker(double scale,
+																			const Eigen::Vector3d &dir,
+																			const QColor &color)
+{
+	visualization_msgs::msg::Marker marker;
+
+	marker.type = visualization_msgs::msg::Marker::ARROW;
+	marker.scale.x = scale;
+	marker.scale.y = 0.1*scale;
+	marker.scale.z = 0.1*scale;
+
+	// set arrow's orientation from given direction
+	updatePose(marker.pose,
+	           Eigen::Quaterniond::FromTwoVectors(Eigen::Vector3d::UnitX(), dir));
+
+	marker.color.r = color.redF();
+	marker.color.g = color.greenF();
+	marker.color.b = color.blueF();
+	marker.color.a = color.alphaF();
+
+	return marker;
+}
+
 /// create an interactive marker located at given position
 void RotationControl::createInteractiveMarker(const Eigen::Vector3d &pos,
                                               const QColor &color) {
@@ -152,6 +176,10 @@ void RotationControl::createInteractiveMarker(const Eigen::Vector3d &pos,
 	visualization_msgs::msg::InteractiveMarkerControl ctrl = createViewPlaneControl();
 	// each control can have multiple markers determining its visual appearance
 	ctrl.markers.push_back(createBoxMarker(3*s, 2*s, 1*s, color));
+	// add an arrow for each axis
+	ctrl.markers.push_back(createArrowMarker(3*s, Eigen::Vector3d::UnitX(), QColor("red")));
+	ctrl.markers.push_back(createArrowMarker(3*s, Eigen::Vector3d::UnitY(), QColor("green")));
+	ctrl.markers.push_back(createArrowMarker(3*s, Eigen::Vector3d::UnitZ(), QColor("blue")));
 
 	// add the control to the interactive marker
 	imarker.controls.push_back(ctrl);
