4fda71f797a69a8415cc0a658ac58aac27ed3372
Robert Haschke <rhaschke@techfak.uni-bielefeld.de>
2020-06-26 14:34:12 +0200

    Hierarchical task control

--- notebook/controller.py
+++ notebook/controller.py
@@ -87,8 +87,7 @@ class Controller(object):
         self.pub = rospy.Publisher('/target_joint_states', JointState, queue_size=10)
         self.joint_msg = JointState()
         self.joint_msg.name = [j.name for j in self.robot.active_joints]
-        self.joint_msg.position = numpy.asarray(
-            [(j.min+j.max)/2 + 0.1*(j.max-j.min)*random.uniform(0, 1) for j in self.robot.active_joints])
+        self.reset()
         self.target_link = pose.child_frame_id
         self.T, self.J = self.robot.fk(self.target_link, dict(zip(self.joint_msg.name, self.joint_msg.position)))
         self.preferred_joints = self.joint_msg.position.copy()
@@ -97,6 +96,10 @@ class Controller(object):
 
         self.im_server = MyInteractiveMarkerServer("controller", self.T)
 
+    def reset(self):
+        self.joint_msg.position = numpy.asarray(
+            [(j.min+j.max)/2 + 0.1*(j.max-j.min)*random.uniform(0, 1) for j in self.robot.active_joints])
+
     def actuate(self, q_delta):
         """Move robot by given changes to joint angles"""
         self.joint_msg.position += q_delta.ravel()  # add (numpy) vector q_delta to current joint position vector
@@ -104,8 +107,8 @@ class Controller(object):
         joints = dict(zip(self.joint_msg.name, self.joint_msg.position))  # turn list of names and joint values into map
         self.T, self.J = self.robot.fk(self.target_link, joints)  # compute new forward kinematics and Jacobian
 
-    def solve(self, J, error):
-        """Inverse velocity kinematics: q_delta = J^+ * error"""
+    def solve(self, tasks):
+        """Hierarchically solve tasks of the form J dq = e"""
         def invert_clip(s):
             return 1./s if s > self.threshold else 0.
 
@@ -115,43 +118,76 @@ class Controller(object):
         def invert_smooth_clip(s):
             return s/(self.threshold**2) if s < self.threshold else 1./s
 
-        # SVD of Mx * J * Mq
-        U, S, Vt = numpy.linalg.svd(self.cartesian_weights[:, None] * J * self.joint_weights[None, :])
-        # compute V'.T = V.T * Mq.T
-        Vt *= self.joint_weights[None, :]
+        N = numpy.identity(tasks[0][0].shape[1])  # nullspace projector of previous tasks
+        JA = numpy.zeros((0, N.shape[0]))  # accumulated Jacobians
+        qdot = numpy.zeros(N.shape[0])
 
-        rank = min(U.shape[0], Vt.shape[1])
-        accepted_singular_values = (S > 1e-3).sum()
-        VN = Vt[accepted_singular_values:].T
-        for i in range(rank):
-            S[i] = invert_smooth_clip(S[i])
+        if isinstance(tasks, tuple):
+            tasks = [tasks]
 
-        qdot1 = numpy.dot(Vt.T[:, 0:rank], S * U.T.dot(self.cartesian_weights * numpy.array(error)))
-        qdot2 = VN.dot(0.1 * VN.T.dot(self.preferred_joints - self.joint_msg.position))
-        return qdot1 + qdot2
+        for J, e in tasks:
+            U, S, Vt = numpy.linalg.svd(J.dot(N) * self.joint_weights[None, :])
+            # compute V'.T = V.T * Mq.T
+            Vt *= self.joint_weights[None, :]
+
+            rank = min(U.shape[0], Vt.shape[1])
+            for i in range(rank):
+                S[i] = invert_smooth_clip(S[i])
+
+            qdot += numpy.dot(Vt.T[:, 0:rank], S * U.T.dot(numpy.array(e) - J.dot(qdot)))
+
+            # compute new nullspace projector
+            JA = numpy.vstack([JA, J])
+            U, S, Vt = numpy.linalg.svd(JA)
+            accepted_singular_values = (S > 1e-3).sum()
+            VN = Vt[accepted_singular_values:].T
+            N = VN.dot(VN.T)
+        return qdot
 
     @staticmethod
-    def position_error(T_tgt, T_cur):
-        """Given homogenous transforms of target and current pose, compute error vector"""
-        return T_tgt[0:3, 3]-T_cur[0:3, 3]
+    def stack(tasks):
+        """Combine all tasks by stacking them into a single Jacobian"""
+        Js, errs = zip(*tasks)
+        return numpy.vstack(Js), numpy.hstack(errs)
 
-    @staticmethod
-    def orientation_error(T_tgt, T_cur):
+    def position_task(self, T_tgt, T_cur, scale=1.0):
+        """Move eef towards a specific target point in base frame"""
+        return self.J[:3], scale*(T_tgt[0:3, 3]-T_cur[0:3, 3])
+
+    def orientation_task(self, T_tgt, T_cur, scale=1.0):
+        """Move eef into a specific target orientation in base frame"""
         delta = numpy.identity(4)
         delta[0:3, 0:3] = T_cur[0:3, 0:3].T.dot(T_tgt[0:3, 0:3])
         angle, axis, _ = tf.rotation_from_matrix(delta)
         # transform rotational velocity from end-effector into base frame orientation (only R!)
-        return T_cur[0:3, 0:3].dot(angle * axis)
+        return self.J[3:], scale*(T_cur[0:3, 0:3].dot(angle * axis))
+
+    def pose_task(self, T_tgt, T_cur, scale=(1.0, 1.0)):
+        """Perform position and orientation task with same priority"""
+        return self.stack([self.position_task(T_tgt, T_cur, scale=scale[0]),
+                           self.orientation_task(T_tgt, T_cur, scale=scale[1])])
+
+    def joint_task(self, scale=0.1):
+        """Move towards a set of preferred joints, consumes all DOFs"""
+        qerr = self.joint_msg.position - self.preferred_joints
+        J = numpy.identity(self.J.shape[1])
+        return J, -scale*qerr
 
     def position_control(self, target):
-        v = self.position_error(target, self.T)
-        q_delta = self.solve(self.J[0:3, :], v)
+        q_delta = self.solve(self.position_task(target, self.T))
         self.actuate(q_delta)
 
     def pose_control(self, target):
-        v = self.position_error(target, self.T)
-        w = self.orientation_error(target, self.T)
-        q_delta = self.solve(self.J, numpy.block([0.1*v, 0.1*w]))
+        q_delta = self.solve(self.pose_task(target, self.T))
+        self.actuate(q_delta)
+
+    def hierarchic_control(self, target):
+        # position control > orientation control > joints
+        q_delta = self.solve([self.position_task(target, self.T),
+                              self.orientation_task(target, self.T),
+                              self.joint_task()])
+        # pose > joints
+        #q_delta = self.solve([self.pose_task(target, self.T), self.joint_task()])
         self.actuate(q_delta)
 
     def lissajous(self, w=0.1*2*numpy.pi, n=2):
@@ -169,5 +205,5 @@ if __name__ == '__main__':
     c = Controller()
     rate = rospy.Rate(50)  # Run control loop at 50 Hz
     while not rospy.is_shutdown():
-        c.pose_control(c.im_server.target)
+        c.hierarchic_control(c.im_server.target)
         rate.sleep()
