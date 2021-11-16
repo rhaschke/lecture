d68ab662417d240d4f7079f5a1440ecd67163f5d
Felix Kolesch <53610070+felixkol@users.noreply.github.com>
2021-11-16 18:00:31 +0100

    fixup! Hierarchical task control
    Handle a single task tuple.

--- notebook/controller.py
+++ notebook/controller.py
@@ -88,6 +88,9 @@ class Controller(object):
         JA = numpy.zeros((0, self.N))  # accumulated Jacobians
         qdot = numpy.zeros(self.N)
 
+        if isinstance(tasks,tuple):
+            tasks = [tasks]
+
         for J, e in tasks:
             U, S, Vt = numpy.linalg.svd(J.dot(N) * self.joint_weights[None, :])
             # compute V'.T = V.T * Mq.T
